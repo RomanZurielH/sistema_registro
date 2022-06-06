1bed3d3d3762d3a72be71c3eb2c580a408311928
RomanZurielH <betterthanyou24awesome@gmail.com>
2022-06-05 23:15:14 -0500

    Formulario registrar proyecto

--- server/routes/projectRouter.js
+++ server/routes/projectRouter.js
@@ -8,12 +8,12 @@ import projectController from '../controllers/projectController';
 const router = new Router();
 
 /* ------ GET ------ */
-// Listar proyector
+// Listade proyectos
 // GET /projects | GET /projects/index
 router.get(['/', '/index'], projectController.index);
 
-// Registrar una idea de proyecto
-// GET /projects/add
+// Envia el formulario para registrar una idea de proyecto
+// get /projects/add
 router.get('/add', projectController.add);
 
 // Exportando el enrutador Projects
