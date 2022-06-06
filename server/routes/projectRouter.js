a3fa68b7bb884296a2946eaa27dbae0b2664b697
RomanZurielH <betterthanyou24awesome@gmail.com>
2022-06-05 23:59:57 -0500

    Validacion de Project con YUP

--- server/routes/projectRouter.js
+++ server/routes/projectRouter.js
@@ -1,6 +1,11 @@
 // Importando el Router de Express
 import { Router } from 'express';
 
+// Importar el validador
+import Validate from '../validators/validateFactory';
+// Importamos el esquema de validación
+import projectValidator from '../validators/projectValidator';
+
 // Importar el controlador de proyectos
 import projectController from '../controllers/projectController';
 
@@ -8,7 +13,7 @@ import projectController from '../controllers/projectController';
 const router = new Router();
 
 /* ------ GET ------ */
-// Listade proyectos
+// Listar proyector
 // GET /projects | GET /projects/index
 router.get(['/', '/index'], projectController.index);
 
@@ -19,7 +24,14 @@ router.get('/add', projectController.add);
 /* ------ POST ------ */
 // Procesa el formulario que Agrega ideas de proyectos
 // POST /projects/add
-router.post('/add', projectController.addPost);
+router.post(
+  '/add',
+  Validate({
+    shape: projectValidator.projectSchema,
+    getObject: projectValidator.getProject,
+  }),
+  projectController.addPost
+);
 
 // Exportando el enrutador Projects
 export default router;
